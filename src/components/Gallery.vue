174d7e30ae864892944d916c372439600e7d6b85
Kevin Nowald <kevin.nowald@applover.pl>
2022-03-02 15:00:12 +0100

    update: Gallery visual style

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -2,7 +2,14 @@
   <div class="container-fluid">
     <div class="row">
       <div class="gallery">
-        <carousel-3d :perspective="0" :space="400" :height="400" :width="400">
+        <carousel-3d
+          :perspective="50"
+          :space="400"
+          :height="400"
+          :width="400"
+          :border="0"
+          :controlsVisible="true"
+        >
           <slide v-for="(item, index) in images" :index="index" :key="index">
             <img :src="getImageUrl(item.split('/').pop())" alt="" />
           </slide>
@@ -34,6 +41,22 @@ export default {
 </script>
 
 <style scoped>
+.carousel-3d-slide {
+  border-radius: 15%;
+  border: none;
+  box-shadow: 15px 15px 5px 0px rgba(0, 0, 0, 0.5);
+  transition: transform 500ms ease 0s, opacity 500ms ease 0s,
+    visibility 500ms ease 0s, box-shadow 0.3s ease-in-out !important;
+}
+
+.carousel-3d-slide.current {
+  box-shadow: 25px 25px 15px 5px rgba(0, 0, 0, 0.5);
+}
+
+.carousel-3d-container {
+  height: 445px !important;
+}
+
 .gallery {
   width: 100%;
   height: 24rem;
